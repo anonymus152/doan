f1965012017dd2ef785688ef977652c75b77148a
anonymus152 <105860337+anonymus152@users.noreply.github.com>
2022-07-04 08:55:46 +0700

    Update antiddos.sh

--- antiddos.sh
+++ antiddos.sh
@@ -5,11 +5,11 @@ iptables -t mangle -X
 if [[ -z $(ip link |grep eth0) ]]
 then
 	read  -p "Nhập IP VPS: " ipaddr
-	iptables -t mangle -A PREROUTING   -d $ipaddr  -p tcp --syn -m multiport --dports 80,443 -m hashlimit --hashlimit-above 300/sec --hashlimit-burst 600 --hashlimit-mode srcip --hashlimit-name synflood_http --hashlimit-htable-max 1048576 --hashlimit-htable-expire 3600000 -m comment --comment "global protect" -j DROP
-	iptables -t mangle -A PREROUTING -d $ipaddr -p tcp -m tcp -m conntrack --ctstate ESTABLISHED -m multiport --dports 80,443 -m connlimit --connlimit-above 100 --connlimit-mask 64 --connlimit-saddr -m comment --comment global -j DROP		 
+	iptables -t mangle -A PREROUTING   -d $ipaddr  -p tcp --syn -m multiport --dports 80,443 -m hashlimit --hashlimit-above 3000/sec --hashlimit-burst 6000 --hashlimit-mode srcip --hashlimit-name synflood_http --hashlimit-htable-max 1048576 --hashlimit-htable-expire 3600000 -m comment --comment "global protect" -j DROP
+	iptables -t mangle -A PREROUTING -d $ipaddr -p tcp -m tcp -m conntrack --ctstate ESTABLISHED -m multiport --dports 80,443 -m connlimit --connlimit-above 1000 --connlimit-mask 640 --connlimit-saddr -m comment --comment global -j DROP		 
 else
-	iptables -t mangle -A PREROUTING  -i eth0 -p tcp --syn -m multiport --dports 80,443 -m hashlimit --hashlimit-above 300/sec --hashlimit-burst 600 --hashlimit-mode srcip --hashlimit-name synflood_http --hashlimit-htable-max 1048576 --hashlimit-htable-expire 3600000 -m comment --comment "global protect" -j DROP
-	iptables -t mangle -A PREROUTING -i eth0 -p tcp -m tcp -m conntrack --ctstate ESTABLISHED -m multiport --dports 80,443 -m connlimit --connlimit-above 100 --connlimit-mask 64 --connlimit-saddr -m comment --comment global -j DROP
+	iptables -t mangle -A PREROUTING  -i eth0 -p tcp --syn -m multiport --dports 80,443 -m hashlimit --hashlimit-above 3000/sec --hashlimit-burst 6000 --hashlimit-mode srcip --hashlimit-name synflood_http --hashlimit-htable-max 1048576 --hashlimit-htable-expire 3600000 -m comment --comment "global protect" -j DROP
+	iptables -t mangle -A PREROUTING -i eth0 -p tcp -m tcp -m conntrack --ctstate ESTABLISHED -m multiport --dports 80,443 -m connlimit --connlimit-above 1000 --connlimit-mask 640 --connlimit-saddr -m comment --comment global -j DROP
 fi
 echo "Done !!!!"
 rm -rf antiddos.sh
